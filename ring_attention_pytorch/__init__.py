a73ec4bac39caf67877bdf9350b5302eb892fdc3
santoshmo <santoshm@stanford.edu>
2025-06-21 15:12:17 -0700

    CMS updates

--- ring_attention_pytorch/__init__.py
+++ ring_attention_pytorch/__init__.py
@@ -10,3 +10,6 @@ from ring_attention_pytorch.ring_flash_attention import (
     ring_flash_attn,
     ring_flash_attn_
 )
+
+# tree attention decoding
+from ring_attention_pytorch.tree_attn_decoding import tree_attn_decode
